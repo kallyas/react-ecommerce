a44b796439c163ff953c930183be266e2f5edd07
Kally <kallyasmedia@gmail.com>
2020-07-15 15:59:39 +0000

    Configure app test script2

--- src/__tests__/integration/app.test.tsx
+++ src/__tests__/integration/app.test.tsx
@@ -8,10 +8,92 @@ import App from '../../components/App'
 import { createStore } from '../../store'
 import { FiltersWrapper } from '../../components/FiltersWrapper'
 
+jest.mock('../../helpers/axios')
+
+const mockAxios = Axios as any
+
+const productBuilder = build('Product', {
+  fields: {
+    id: fake((f) => f.random.number()),
+    image: fake((f) => f.image.imageUrl()),
+    name: fake((f) => f.lorem.words()),
+    price: fake((f) => `from $${f.random.number(100)}`),
+  },
+})
+
 describe('The app ', () => {
-  test('❌ it fetches and renders all products on the page', async () => {})
+  const setupApp = () =>
+    render(
+      <StoreProvider store={createStore()}>
+        <FiltersWrapper>
+          <App />
+        </FiltersWrapper>
+      </StoreProvider>,
+    )
 
-  test('❌ it can open and close the filters panel', async () => {})
+  afterEach(() => jest.clearAllMocks())
 
-  test('❌ it can search products as user types in the search field', async () => {})
+  test('it fetches and renders all products on the page', async () => {
+    mockAxios.get.mockResolvedValue({
+      data: [productBuilder(), productBuilder()],
+    })
+    const { findAllByTestId } = setupApp()
+
+    expect(await findAllByTestId('ProductTile')).toHaveLength(2)
+  })
+
+  test('it can open and close the filters panel', async () => {
+    const { getByText, queryByText } = setupApp()
+
+    const filterButton = getByText(/filter/i)
+
+    expect(queryByText(/reset to defaults/i)).not.toBeInTheDocument()
+
+    fireEvent.click(filterButton)
+
+    expect(queryByText(/reset to defaults/i)).toBeInTheDocument()
+
+    fireEvent.click(getByText(/view results/i))
+
+    expect(queryByText(/reset to defaults/i)).not.toBeInTheDocument()
+    expect(queryByText(/view results/i)).not.toBeInTheDocument()
+  })
+
+  test('it can search products as user types in the search field', async () => {
+    jest.useFakeTimers()
+    mockAxios.get
+      .mockResolvedValueOnce({
+        data: [
+          productBuilder(),
+          productBuilder(),
+          productBuilder(),
+          productBuilder(),
+          productBuilder(),
+        ],
+      })
+      .mockResolvedValueOnce({
+        data: [productBuilder(), productBuilder()],
+      })
+    const { findAllByTestId, getByText, getByPlaceholderText } = setupApp()
+
+    expect(await findAllByTestId('ProductTile')).toHaveLength(5)
+
+    fireEvent.click(getByText(/filter/i))
+
+    const searchBox = getByPlaceholderText(/largo/i)
+
+    fireEvent.change(searchBox, {
+      target: {
+        value: 'searching',
+      },
+    })
+
+    act(() => {
+      jest.runAllTimers()
+    })
+
+    await waitFor(() => expect(mockAxios.get).toHaveBeenCalledTimes(2))
+
+    expect(await findAllByTestId('ProductTile')).toHaveLength(2)
+  })
 })
